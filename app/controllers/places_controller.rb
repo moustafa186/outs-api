76d4a80fe83232ff38554528be3fd935661bb664
Moustafa Kandeel <moustafa186@gmail.com>
2018-02-25 13:17:13 +0200

    places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,10 +11,25 @@ class PlacesController < ApplicationController
   end
 
   def create
-    # @place = Place.create
-    # render json: @place, status: created
+    @place = Place.new(place_params)
+    if @place.save
+        render json: @place, status: :created, location: @place
+      else
+        render json: @place.errors, status: :unprocessable_entity
+      end
   end
 
   def update
+    @place = Place.find(params[:id])
+    if @place.update(place_params)
+        render json: @place, status: :ok, location: @place
+      else
+        render json: @place.errors, status: :unprocessable_entity
+      end
+  end
+
+  private 
+  def place_params
+    ActiveModelSerializers::Deserialization.jsonapi_parse!(params.to_unsafe_h)
   end
 end
