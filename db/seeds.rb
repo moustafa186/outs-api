4874814a887a8936844e65c90924fd871d55e198
Moustafa Kandeel <moustafa186@gmail.com>
2018-02-26 19:16:50 +0200

    db seeds

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Place.create!(name: "Abou Mazen", category: "Resturant", budget: 20.0, rating: 4.8)
+Place.create!(name: "Greek Campus", category: "WorkSpace", budget: 0.0, rating: 4.9)
+Place.create!(name: "NUTechSpace", category: "Incubator", budget: 0.0, rating: 5.0)
